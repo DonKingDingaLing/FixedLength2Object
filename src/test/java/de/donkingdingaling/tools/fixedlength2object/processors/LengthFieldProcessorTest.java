b2a116a170117646308be6661cc900b24668eab5
DonKingDingaLing <dominik-gruber@live.de>
2021-09-21 00:59:01 +0200

    Restructured fixed length operations

--- src/test/java/de/donkingdingaling/tools/fixedlength2object/processors/LengthFieldProcessorTest.java
+++ src/test/java/de/donkingdingaling/tools/fixedlength2object/processors/LengthFieldProcessorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class FixedLengthFieldProcessorTest {
+class LengthFieldProcessorTest {
     @BeforeEach
     void setUp() {
     }
